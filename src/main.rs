02966eddc6bb7d01d06f8a955d2896c700075e0f
Not Alex <alex@totallynotmyemail.com>
2023-01-21 01:02:38 +0100

    Alex is a Rust genius, Mo came up with the stupid idea

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate termios;
 pub mod hardware;
 use hardware::vm::VM;
 
-use termios::*;
-
 use byteorder::{BigEndian, ReadBytesExt};
 
 use std::{fs::File, io::BufReader};
@@ -14,10 +12,7 @@ use structopt::StructOpt;
 struct Cli {
     /// The path to the file to read
     #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-
-    #[structopt(long)]
-    print_asm: bool, // Future feature
+    path: Vec<std::path::PathBuf>,
 }
 
 fn main() {
@@ -26,30 +21,18 @@ fn main() {
 
     let cli = Cli::from_args();
 
-    let f = File::open(cli.path).expect("couldn't open file");
-    let mut f = BufReader::new(f);
-
-    // Note how we're using `read_u16` _and_ BigEndian to read the binary file.
-    let base_address = f.read_u16::<BigEndian>().expect("error");
-
-    // Here we're loading the program in memory
-    let mut address = base_address as usize;
-    loop {
-        match f.read_u16::<BigEndian>() {
-            Ok(instruction) => {
-                vm.write_memory(address, instruction);
-                address += 1;
-            }
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::UnexpectedEof {
-                    println!("OK, EOF reached");
-                } else {
-                    println!("failed: {}", e);
-                }
-                break;
-            }
+    for path in cli.path {
+        let file = File::open(path.clone()).unwrap();
+        let mut reader = BufReader::new(file);
+        let mut address = reader.read_u16::<BigEndian>().unwrap();
+        while let Ok(value) = reader.read_u16::<BigEndian>() {
+            vm.write_memory(address as usize, value);
+            address += 1;
         }
+
+        println!("Loaded file {} into memory", path.display());
     }
+
 
     println!("Loaded program in memory");
     hardware::execute_program(&mut vm);
